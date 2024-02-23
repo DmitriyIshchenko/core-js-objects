fbe263ca377ccc1883d028fbd7a2a234c99fabb2
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-02-23 22:23:47 +0300

    feat: sovle sortCitiesArray problem

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -215,8 +215,12 @@ function fromJSON(proto, json) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  return arr.sort((a, b) =>
+    a.country.localeCompare(b.country) === 0
+      ? a.city.localeCompare(b.city)
+      : a.country.localeCompare(b.country)
+  );
 }
 
 /**
